6e0bf8e56686130c6016f083322e327fe0d61258
julesrx <dev@julesrx.fr>
2023-11-16 18:36:59 +0100

    publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-npm publish --access public
+bump=$(npm version patch)
+echo "Bumped version to $bump"
+
+read -p "Are you sure? " -n 1 -r
+echo   
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    npm publish --access public
+fi
