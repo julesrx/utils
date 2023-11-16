931200b9f8b4c3369f19531d4aa72ee0367f632f
julesrx <dev@julesrx.fr>
2023-11-16 17:50:14 +0100

    repo

--- src/dates.ts
+++ src/dates.ts
@@ -1,24 +1,24 @@
 export const relativeDateFormatter = new Intl.RelativeTimeFormat();
 
-// const dateDivisions: { amount: number; name: Intl.RelativeTimeFormatUnit }[] = [
-//     { amount: 60, name: 'seconds' },
-//     { amount: 60, name: 'minutes' },
-//     { amount: 24, name: 'hours' },
-//     { amount: 7, name: 'days' },
-//     { amount: 4.34524, name: 'weeks' },
-//     { amount: 12, name: 'months' },
-//     { amount: Number.POSITIVE_INFINITY, name: 'years' },
-// ];
+const dateDivisions: { amount: number; name: Intl.RelativeTimeFormatUnit }[] = [
+    { amount: 60, name: 'seconds' },
+    { amount: 60, name: 'minutes' },
+    { amount: 24, name: 'hours' },
+    { amount: 7, name: 'days' },
+    { amount: 4.34524, name: 'weeks' },
+    { amount: 12, name: 'months' },
+    { amount: Number.POSITIVE_INFINITY, name: 'years' },
+];
 
-// export const formatTimeAgo = (date: Date) => {
-//     const now = new Date();
-//     let duration = (date.getTime() - now.getTime()) / 1000;
+export const formatTimeAgo = (date: Date) => {
+    const now = new Date();
+    let duration = (date.getTime() - now.getTime()) / 1000;
 
-//     for (let i = 0; i < dateDivisions.length; i++) {
-//         const division = dateDivisions[i];
-//         if (Math.abs(duration) < division.amount) {
-//             return relativeDateFormatter.format(Math.round(duration), division.name);
-//         }
-//         duration /= division.amount;
-//     }
-// };
+    for (let i = 0; i < dateDivisions.length; i++) {
+        const division = dateDivisions[i];
+        if (Math.abs(duration) < division.amount) {
+            return relativeDateFormatter.format(Math.round(duration), division.name);
+        }
+        duration /= division.amount;
+    }
+};
