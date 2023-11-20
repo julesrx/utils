08697454807026f07b9a5d008d9346291ebb9470
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2023-11-20 17:45:01 +0100

    feat: cache storage with unstorage (#1)
    * add cache
    
    * cache ok
    
    * tests

---
+++ src/cache.ts
@@ -0,0 +1,67 @@
+import { createStorage, type Driver } from 'unstorage';
+import ibb from 'unstorage/drivers/indexedb';
+
+interface CacheItem<T> {
+    value: T;
+    expiration?: number;
+}
+
+interface CacheStorageOptions {
+    driver?: Driver;
+    base?: string;
+}
+
+export interface CacheStorage {
+    get: <T>(id: string) => Promise<T | null>;
+    set: <T>(id: string, value: T, secondsExpiration?: number) => Promise<void>;
+    gset: <T>(
+        id: string,
+        getter: () => T | Promise<T>,
+        secondsExpiration?: number
+    ) => Promise<T | null>;
+}
+
+export const createCacheStorage = ({
+    driver,
+    base = 'cache',
+}: CacheStorageOptions = {}): CacheStorage => {
+    const storage = createStorage({ driver: driver ?? ibb({ base }) });
+
+    const get = async <T>(id: string) => {
+        const item = await storage.getItem<CacheItem<T>>(id);
+        if (!item) return null;
+
+        if (item.expiration && item.expiration <= Date.now()) {
+            await storage.removeItem(id);
+            return null;
+        }
+
+        return item.value;
+    };
+
+    const set = async <T>(id: string, value: T, secondsExpiration?: number) => {
+        secondsExpiration = secondsExpiration ?? 60 * 30;
+
+        const now = Date.now();
+        const expiration = now + secondsExpiration * 1000;
+
+        const item: CacheItem<T> = { value, expiration };
+        await storage.setItem(id, item);
+    };
+
+    const gset = async <T>(
+        id: string,
+        getter: () => T | Promise<T>,
+        secondsExpiration?: number
+    ) => {
+        const cached = await get<T>(id);
+        if (cached) return cached;
+
+        const res = await getter();
+
+        await set(id, res, secondsExpiration);
+        return res;
+    };
+
+    return { get, set, gset };
+};
