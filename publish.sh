f190b90515cd55bc7ba047e6b83f5c024a1b9684
julesrx <dev@julesrx.fr>
2023-11-20 17:48:39 +0100

    fix: ask before bumping version in publish script

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
-bump=$(npm version patch)
-echo "Bumped version to $bump"
-
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? y/n" -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    bump=$(npm version patch)
+    echo "Bumped version to $bump"
+
     npm publish --access public
+    git push
 fi
