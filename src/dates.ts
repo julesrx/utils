6e8922fb0e15aa37a4ff945e7a0a20e15fe6dabd
Jules Raffoux <25800503+julesrx@users.noreply.github.com>
2024-07-05 00:24:11 +0200

    chore: update deps (#5)

--- src/dates.ts
+++ src/dates.ts
@@ -3,83 +3,83 @@ type Locales = string | string[] | undefined;
 type UnitDisplay = 'short' | 'long' | 'narrow' | undefined;
 
 const dateDivisions: Division[] = [
-    { amount: 60, name: 'seconds' },
-    { amount: 60, name: 'minutes' },
-    { amount: 24, name: 'hours' },
-    { amount: 7, name: 'days' },
-    { amount: 4.34524, name: 'weeks' },
-    { amount: 12, name: 'months' },
-    { amount: Number.POSITIVE_INFINITY, name: 'years' },
+  { amount: 60, name: 'seconds' },
+  { amount: 60, name: 'minutes' },
+  { amount: 24, name: 'hours' },
+  { amount: 7, name: 'days' },
+  { amount: 4.34524, name: 'weeks' },
+  { amount: 12, name: 'months' },
+  { amount: Number.POSITIVE_INFINITY, name: 'years' }
 ];
 
 export const createTimeAgoFormatter = (locales?: Locales) => {
-    const relativeDateFormatter = new Intl.RelativeTimeFormat(locales);
+  const relativeDateFormatter = new Intl.RelativeTimeFormat(locales);
 
-    return {
-        format: (date: Date) => {
-            const now = new Date();
-            let duration = (date.getTime() - now.getTime()) / 1000;
+  return {
+    format: (date: Date) => {
+      const now = new Date();
+      let duration = (date.getTime() - now.getTime()) / 1000;
 
-            for (let i = 0; i < dateDivisions.length; i++) {
-                const division = dateDivisions[i];
-                if (Math.abs(duration) < division.amount) {
-                    return relativeDateFormatter.format(Math.round(duration), division.name);
-                }
-                duration /= division.amount;
-            }
-        },
-    };
+      for (let i = 0; i < dateDivisions.length; i++) {
+        const division = dateDivisions[i];
+        if (Math.abs(duration) < division.amount) {
+          return relativeDateFormatter.format(Math.round(duration), division.name);
+        }
+        duration /= division.amount;
+      }
+    }
+  };
 };
 
 const divMod = (n: number, m: number) => [Math.floor(n / m), n % m];
 
 interface DurationFormatterOptions {
-    unitDisplay?: UnitDisplay;
-    listStyle?: Intl.ListFormatStyle;
-    showMilliseconds?: boolean;
+  unitDisplay?: UnitDisplay;
+  listStyle?: Intl.ListFormatStyle;
+  showMilliseconds?: boolean;
 }
 
 export const createDurationFormatter = (
-    locales?: Locales,
-    {
-        unitDisplay = 'long',
-        listStyle = 'long',
-        showMilliseconds = false,
-    }: DurationFormatterOptions = {}
+  locales?: Locales,
+  {
+    unitDisplay = 'long',
+    listStyle = 'long',
+    showMilliseconds = false
+  }: DurationFormatterOptions = {}
 ) => {
-    const timeUnitFormatter = (locale: Locales, unit: string, unitDisplay: UnitDisplay) => {
-        return Intl.NumberFormat(locale, { style: 'unit', unit, unitDisplay }).format;
-    };
+  const timeUnitFormatter = (locale: Locales, unit: string, unitDisplay: UnitDisplay) => {
+    return Intl.NumberFormat(locale, { style: 'unit', unit, unitDisplay }).format;
+  };
 
-    const fmtDays = timeUnitFormatter(locales, 'day', unitDisplay);
-    const fmtHours = timeUnitFormatter(locales, 'hour', unitDisplay);
-    const fmtMinutes = timeUnitFormatter(locales, 'minute', unitDisplay);
-    const fmtSeconds = timeUnitFormatter(locales, 'second', unitDisplay);
-    const fmtMilliseconds = timeUnitFormatter(locales, 'millisecond', unitDisplay);
+  const fmtDays = timeUnitFormatter(locales, 'day', unitDisplay);
+  const fmtHours = timeUnitFormatter(locales, 'hour', unitDisplay);
+  const fmtMinutes = timeUnitFormatter(locales, 'minute', unitDisplay);
+  const fmtSeconds = timeUnitFormatter(locales, 'second', unitDisplay);
+  const fmtMilliseconds = timeUnitFormatter(locales, 'millisecond', unitDisplay);
 
-    const listFormatter = new Intl.ListFormat(locales, {
-        style: listStyle,
-        type: 'conjunction',
-    });
+  const listFormatter = new Intl.ListFormat(locales, {
+    style: listStyle,
+    type: 'conjunction'
+  });
 
-    return {
-        format: (milliseconds: number) => {
-            let days, hours, minutes, seconds;
+  return {
+    format: (milliseconds: number) => {
+      let days, hours, minutes, seconds;
 
-            [days, milliseconds] = divMod(milliseconds, 864e5);
-            [hours, milliseconds] = divMod(milliseconds, 36e5);
-            [minutes, milliseconds] = divMod(milliseconds, 6e4);
-            [seconds, milliseconds] = divMod(milliseconds, 1e3);
+      [days, milliseconds] = divMod(milliseconds, 864e5);
+      [hours, milliseconds] = divMod(milliseconds, 36e5);
+      [minutes, milliseconds] = divMod(milliseconds, 6e4);
+      [seconds, milliseconds] = divMod(milliseconds, 1e3);
 
-            const formats = [
-                days ? fmtDays(days) : '',
-                hours ? fmtHours(hours) : '',
-                minutes ? fmtMinutes(minutes) : '',
-                seconds ? fmtSeconds(seconds) : '',
-                showMilliseconds && milliseconds ? fmtMilliseconds(milliseconds) : '',
-            ];
+      const formats = [
+        days ? fmtDays(days) : '',
+        hours ? fmtHours(hours) : '',
+        minutes ? fmtMinutes(minutes) : '',
+        seconds ? fmtSeconds(seconds) : '',
+        showMilliseconds && milliseconds ? fmtMilliseconds(milliseconds) : ''
+      ];
 
-            return listFormatter.format(formats.filter((v) => v !== ''));
-        },
-    };
+      return listFormatter.format(formats.filter(v => v !== ''));
+    }
+  };
 };
--- tests/dates.test.ts
+++ tests/dates.test.ts
@@ -4,92 +4,92 @@ import { describe, expect, test, vi } from 'vitest';
 const locale = 'en-US';
 
 describe('time ago formatter', () => {
-    const timeAgoFormatter = createTimeAgoFormatter(locale);
+  const timeAgoFormatter = createTimeAgoFormatter(locale);
 
-    test('format seconds ago', () => {
-        var now = new Date(2023, 11, 16, 12, 34, 16);
-        var then = new Date(2023, 11, 16, 12, 34, 10);
+  test('format seconds ago', () => {
+    var now = new Date(2023, 11, 16, 12, 34, 16);
+    var then = new Date(2023, 11, 16, 12, 34, 10);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('6 seconds ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('6 seconds ago');
+  });
 
-    test('format minutes ago', () => {
-        var now = new Date(2023, 11, 16, 12, 49);
-        var then = new Date(2023, 11, 16, 12, 34);
+  test('format minutes ago', () => {
+    var now = new Date(2023, 11, 16, 12, 49);
+    var then = new Date(2023, 11, 16, 12, 34);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('15 minutes ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('15 minutes ago');
+  });
 
-    test('format hours ago', () => {
-        var now = new Date(2023, 11, 16, 19);
-        var then = new Date(2023, 11, 16, 12);
+  test('format hours ago', () => {
+    var now = new Date(2023, 11, 16, 19);
+    var then = new Date(2023, 11, 16, 12);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('7 hours ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('7 hours ago');
+  });
 
-    test('format days ago', () => {
-        var now = new Date(2023, 11, 16);
-        var then = new Date(2023, 11, 13);
+  test('format days ago', () => {
+    var now = new Date(2023, 11, 16);
+    var then = new Date(2023, 11, 13);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('3 days ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('3 days ago');
+  });
 
-    test('format weeks ago', () => {
-        var now = new Date(2023, 11, 27);
-        var then = new Date(2023, 11, 13);
+  test('format weeks ago', () => {
+    var now = new Date(2023, 11, 27);
+    var then = new Date(2023, 11, 13);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('2 weeks ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('2 weeks ago');
+  });
 
-    test('format months ago', () => {
-        var now = new Date(2024, 3);
-        var then = new Date(2023, 11);
+  test('format months ago', () => {
+    var now = new Date(2024, 3);
+    var then = new Date(2023, 11);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('4 months ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('4 months ago');
+  });
 
-    test('format years ago', () => {
-        var now = new Date(2027, 3);
-        var then = new Date(2023, 11);
+  test('format years ago', () => {
+    var now = new Date(2027, 3);
+    var then = new Date(2023, 11);
 
-        vi.setSystemTime(now);
+    vi.setSystemTime(now);
 
-        expect(timeAgoFormatter.format(then)).toBe('3 years ago');
-    });
+    expect(timeAgoFormatter.format(then)).toBe('3 years ago');
+  });
 });
 
 describe.concurrent('duration formatter', () => {
-    const durationFormatter = createDurationFormatter(locale);
-    const shortFormatter = createDurationFormatter(locale, {
-        unitDisplay: 'short',
-        listStyle: 'narrow',
-    });
-    const withMillisecondsFormatter = createDurationFormatter(locale, { showMilliseconds: true });
+  const durationFormatter = createDurationFormatter(locale);
+  const shortFormatter = createDurationFormatter(locale, {
+    unitDisplay: 'short',
+    listStyle: 'narrow'
+  });
+  const withMillisecondsFormatter = createDurationFormatter(locale, { showMilliseconds: true });
 
-    test('format 1m', () => expect(durationFormatter.format(6e4)).toBe('1 minute'));
+  test('format 1m', () => expect(durationFormatter.format(6e4)).toBe('1 minute'));
 
-    test('format 1h40m', () => expect(durationFormatter.format(6e6)).toBe('1 hour and 40 minutes'));
+  test('format 1h40m', () => expect(durationFormatter.format(6e6)).toBe('1 hour and 40 minutes'));
 
-    test('format 2h21m', () =>
-        expect(durationFormatter.format(846e4)).toBe('2 hours and 21 minutes'));
+  test('format 2h21m', () =>
+    expect(durationFormatter.format(846e4)).toBe('2 hours and 21 minutes'));
 
-    test('format 1d19h49s', () =>
-        expect(durationFormatter.format(154849e3)).toBe('1 day, 19 hours, and 49 seconds'));
+  test('format 1d19h49s', () =>
+    expect(durationFormatter.format(154849e3)).toBe('1 day, 19 hours, and 49 seconds'));
 
-    test('format short 1h19m', () => expect(shortFormatter.format(846e4)).toBe('2 hr, 21 min'));
+  test('format short 1h19m', () => expect(shortFormatter.format(846e4)).toBe('2 hr, 21 min'));
 
-    test('format with milliseconds 5ms', () =>
-        expect(withMillisecondsFormatter.format(5)).toBe('5 milliseconds'));
+  test('format with milliseconds 5ms', () =>
+    expect(withMillisecondsFormatter.format(5)).toBe('5 milliseconds'));
 });
